da318bf77240496c8ebe5ce6c8a4ea30894f5887
vipmarwah <vipal.marwah@untribe.com>
2024-10-17 09:36:35 -0600

    removed logs

--- dags/submit_sparkjob.py
+++ dags/submit_sparkjob.py
@@ -12,16 +12,31 @@ dag = DAG(
     schedule_interval=None,
 )
 
-submit_spark_app = KubernetesPodOperator(
-    namespace='default',
-    name="submit-spark-job",
-    task_id="submit-spark-app",
-    is_delete_operator_pod=True,
-    in_cluster=True,
-    dag=dag,
-    cmds=["kubectl", "apply", "-f", "https://github.com/vipmarwah/sparkdags/sparkapp.yaml"],
-    image="bitnami/kubectl:latest",
-    #get_logs=True,
-)
+k8s_task = KubernetesPodOperator(
+        namespace='default',  # Kubernetes namespace
+        image="python:3.8-slim",  # Docker image to use
+        cmds=["python", "-c"],
+        arguments=[
+            "print('Hello from Kubernetes Pod Operator!')"
+        ],
+        labels={"foo": "bar"},  # Optional labels
+        name="airflow-test-pod",  # Name of the pod
+        task_id="run_pod",
+        get_logs=True,  # Capture pod logs and display in Airflow
+        is_delete_operator_pod=True,  # Delete pod after task completion
+        in_cluster=True,  # Run within the cluster
+        do_xcom_push=False,  # Disable XCom if not needed
+    )
+# submit_spark_app = KubernetesPodOperator(
+#     namespace='default',
+#     name="submit-spark-job",
+#     task_id="submit-spark-app",
+#     is_delete_operator_pod=True,
+#     in_cluster=True,
+#     dag=dag,
+#     cmds=["kubectl", "apply", "-f", "https://github.com/vipmarwah/sparkdags/sparkapp.yaml"],
+#     image="bitnami/kubectl:latest",
+#     #get_logs=True,
+# )
 
 submit_spark_app
